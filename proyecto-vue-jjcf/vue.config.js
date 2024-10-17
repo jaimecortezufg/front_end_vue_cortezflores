39047e643224ac0d9b8cb5c2a957d8c94eb31cdf
Jaime Jeovanny Cortez Flores <jaimecortez@ufg.edu.sv>
2024-10-17 08:07:09 -0600

    Desarrollo de FrontEnd

---
+++ proyecto-vue-jjcf/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+/*module.exports = defineConfig({
+  transpileDependencies: true
+})*/
+module.exports = {
+  pwa:{
+    name: "Proyecto de Jaime Jeovanny Cortez Flores",
+    themeColor: "#FFD100",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBar: "Black"
+  }
+}
